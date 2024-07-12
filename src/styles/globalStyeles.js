8fc0d8a1b9f98f76759e5705563185d042381825
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-07-12 09:26:38 +0900

    css: globalStyles

--- src/styles/globalStyeles.js
+++ src/styles/globalStyeles.js
@@ -8,15 +8,15 @@ const GlobalStyles = createGlobalStyle`
     *{
         box-sizing: border-box;
     }
-    html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
-    a, dl, dt, dd, ol, ul, li, form, label, table{
-        margin: 0;
-        padding: 0;
-        border: 0;
-        font-size: 10px;
-        vertical-align: baseline;
-        overflow: hidden;
-    }
+    //html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
+    //a, dl, dt, dd, ol, ul, li, form, label, table{
+    //    margin: 0;
+    //    padding: 0;
+    //    border: 0;
+    //    font-size: 10px;
+    //    vertical-align: baseline;
+    //    overflow: hidden;
+    //}
     body{
         line-height: 1;
         font-family: 'Noto Sans KR', sans-serif;
